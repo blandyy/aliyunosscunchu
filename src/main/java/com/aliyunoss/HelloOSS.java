cc65ce7b8975e1892fde4219b1c3a2602bb399db
muqi <bland107434@163.com>
2018-03-18 19:19:05 +0800

    first commit

--- src/main/java/com/aliyunoss/HelloOSS.java
+++ src/main/java/com/aliyunoss/HelloOSS.java
@@ -18,21 +18,21 @@ public class HelloOSS {
     // 链接地址是：https://help.aliyun.com/document_detail/oss/user_guide/oss_concept/endpoint.html?spm=5176.docoss/user_guide/endpoint_region
     // endpoint的格式形如“http://oss-cn-hangzhou.aliyuncs.com/”，注意http://后不带bucket名称，
     // 比如“http://bucket-name.oss-cn-hangzhou.aliyuncs.com”，是错误的endpoint，请去掉其中的“bucket-name”。
-    private static String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
+    private static String endpoint = "";
 
     // accessKeyId和accessKeySecret是OSS的访问密钥，您可以在控制台上创建和查看，
     // 创建和查看访问密钥的链接地址是：https://ak-console.aliyun.com/#/。
     // 注意：accessKeyId和accessKeySecret前后都没有空格，从控制台复制时请检查并去除多余的空格。
-    private static String accessKeyId = "LTAIzN0OHqWMc82v";
-    private static String accessKeySecret = "7NhNS3hwuvm3o9v09jQNOAIbFOxbkr";
+    private static String accessKeyId = "";
+    private static String accessKeySecret = "";
 
     // Bucket用来管理所存储Object的存储空间，详细描述请参看“开发人员指南 > 基本概念 > OSS基本概念介绍”。
     // Bucket命名规范如下：只能包括小写字母，数字和短横线（-），必须以小写字母或者数字开头，长度必须在3-63字节之间。
-    private static String bucketName = "muqi-one";
+    private static String bucketName = "";
 
     // Object是OSS存储数据的基本单元，称为OSS的对象，也被称为OSS的文件。详细描述请参看“开发人员指南 > 基本概念 > OSS基本概念介绍”。
     // Object命名规范如下：使用UTF-8编码，长度必须在1-1023字节之间，不能以“/”或者“\”字符开头。
-    private static String firstKey = "onekey";
+    private static String firstKey = "";
 
     public void HiOSS() {
         // 日志配置，OSS Java SDK使用log4j记录错误信息。会在工程目录下生成“oss.log”日志文件，默认日志级别是INFO。
